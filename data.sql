45e2eabbbe71bf61df6eb33e38e2c6fc678873b4
shuanhanhuang <100658839+shuanhanhuang@users.noreply.github.com>
2024-06-08 18:57:59 +0800

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,9 @@
+-- esum project
+
+create database esum;
+
+use esum;
+
+INSERT INTO user (password, username) VALUES ("admin","admin"), ("esum123","esum"), ("alan123","alan"), ("han123","han");
+
+INSERT INTO shop (name, pid, price, quantity) VALUES ("osii 舒壓按摩椅","P001",98000,5), ("網友最愛起司蛋糕","P002",1200,50), ("真愛密碼項鍊","P003",8500,20);
